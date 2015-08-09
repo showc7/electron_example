fec5ce28a38a7ba734bd3b3823d7168e1967023e
Kirill Belov <showc7@gmail.com>
2015-08-09 22:15:17 +0300

    loading from file

--- scripts/config.js
+++ scripts/config.js
@@ -1,19 +1,42 @@
 window.$ = require('jquery-deferred');
 window._ = require('lodash');
 
+var fs = require('fs');
+
 (function (global){
     var container;
 
     function Config() {
     }
 
-    Config.prototype.save = function() {
-        return JSON.stringify(container);
+    Config.prototype.save = function(fileName) {
+        var data = JSON.stringify(container);
+        fs.writeFile(fileName, data, function(err) {
+          if(err) {
+            // call error message
+            console.log('error saving config' + err);
+            return;
+          }
+          // OK
+        });
     };
 
-    Config.prototype.load = function() {
-        container = { 'habrahabr' : 'http://habrahabr.ru/rss/interesting/' };
-        return (new $.Deferred()).resolve().promise();
+    Config.prototype.load = function(fileName) {
+        var dfd = $.Deferred();
+        console.log(fileName);
+        fs.readFile(fileName, function read(err, data){
+          if(err) {
+            container = {};
+            return dfd.reject();
+          }
+          console.log(data);
+          container = JSON.parse(data);
+          console.log(container);
+          return dfd.resolve();
+        });
+        //container = JSON.parse(fs.readFileSync(fileName));
+        //container = { 'habrahabr' : 'http://habrahabr.ru/rss/interesting/' };
+        return dfd;
     };
 
     Config.prototype.get = function(k) {
@@ -22,7 +45,7 @@ window._ = require('lodash');
     };
 
     Config.prototype.set = function(k, v) {
-        // body...
+        container[k] = v;
     };
 
     global.Config = Config;
