bf361505d8fae1697f7ebfb911f8352caa1e45d7
Kirill Belov <showc7@gmail.com>
2015-08-09 22:16:04 +0300

    change render errors

--- scripts/dispatcher.js
+++ scripts/dispatcher.js
@@ -1,9 +1,10 @@
 (function (global) {
     global.Dispatcher = function Dispatcher(options) {
-       
+
     }
 
     Dispatcher.prototype.onclick = function (event) {
         app.loadFeed(event.target.innerHTML);
+        app.renderer.renderError('test error');
     };
 })(window);
