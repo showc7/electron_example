504d8e9af61d9fd8ca6d8f4b221cc9429cdcf5a7
Kirill Belov <showc7@gmail.com>
2015-08-10 10:37:11 +0300

    switch for different buttons

--- scripts/dispatcher.js
+++ scripts/dispatcher.js
@@ -4,7 +4,13 @@
     }
 
     Dispatcher.prototype.onclick = function (event) {
-        app.loadFeed(event.target.innerHTML);
-        app.renderer.renderError('test error');
+        console.log('click');
+        var element = event.target;
+
+        switch(element.getAttribute('data-type')) {
+          case 'feed-url': app.loadFeed(event.target.getAttribute('data-url')); break;
+          case 'back': app.back(); break;
+          default: return;
+        }
     };
 })(window);
